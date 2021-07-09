fe60359c04371f59b12e186206b749c363f1bbc8
Ryan Jeffares <ryandjeffares@gmail.com>
2021-07-09 02:02:57 +0100

    First commit

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,6 @@ pub mod processors;
 pub mod utilities;
 pub mod consts;
 
-fn main() {
-    let adsr = utilities::adsr::ADSR::new(0.0, 0.0, 1.0, 0.0, 44100.0);
-}
 #[cfg(test)]
 mod tests {    
     #[test]
